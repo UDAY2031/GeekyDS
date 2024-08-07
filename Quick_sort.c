7a637a190c7c4bb6a84c692c7f0bec9b908b29e1
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2024-08-07 18:13:39 +0530

    Update Quick_sort.c

--- Quick_sort.c
+++ Quick_sort.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-// Function to swap two elements
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b){
+    int t = *a; 
+    *a = *b; 
+    *b = t;
 }
 
-// Partition function
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
+int partition(int arr[], int low, int high){
+    int pivot = arr[high], i = low - 1;
+    for (int j = low; j < high; j++){
+        if (arr[j] < pivot)
+            swap(&arr[++i], &arr[j]);
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+    swap(&arr[++i], &arr[high]);
+    return i;
 }
 
-// Quicksort function
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
@@ -33,49 +25,15 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
-// Function to generate random numbers
-void generateRandomNumbers(int arr[], int size) {
-    srand(time(0));  // Seed the random number generator
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 1000;  // Generate numbers between 0 and 999
-    }
-}
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
 int main() {
-    int size;
-    printf("Enter the number of elements: ");
-    scanf("%d", &size);
+    int arr[] = {10, 7, 8, 9, 1, 5}, n = 6;
 
-    int arr[size];  // Variable-length array (VLA) to store elements
-
-    // Generate random numbers and print the unsorted array
-    generateRandomNumbers(arr, size);
-    printf("Unsorted array:\n");
-    printArray(arr, size);
-
-    // Measure execution time of sorting
-    clock_t start, end;
-    double cpu_time_used;
-
-    start = clock();
-    quickSort(arr, 0, size - 1);
-    end = clock();
-
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    // Print sorted array and execution time
-    printf("\nSorted array:\n");
-    printArray(arr, size);
-    printf("\nExecution time: %f seconds\n", cpu_time_used);
-
+    clock_t start,end;
+    start=clock();
+    quickSort(arr, 0, n - 1);
+    end=clock();
+    printf("Time taken by Quick Sort : %f seconds\n",(float)(end-start)/CLOCKS_PER_SEC);
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     return 0;
 }
-
